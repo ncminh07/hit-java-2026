082f850c2ec17bd77a5eb13711386af5df1ac61b
ncminh07 <ncminh250107@gmail.com>
2026-07-15 15:36:18 +0700

    baitapbuoi2

---
+++ bai1/src/bai5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class bai4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so vao day cho t : ");
+        String so = scanner.nextLine();
+        int a = Double.parseDouble(so);
+        for(int i  )
+        System.out.print(so);
+
+    }
+}
